8558b4e9f0250ea30ea4ea12f0be0eb631da802e
Julián García <julian.gabus@gmail.com>
2018-03-05 02:33:38 -0600

    first commit

--- main.py
+++ main.py
@@ -11,29 +11,33 @@ def get_tree(url):
     return tree
 
 
-xtree = get_tree('https://staterecords.org/sitemap1.xml')
-urls = [el.text for el in xtree.xpath('//loc')]
-print len(urls)
-no_repeats = list(set(urls))
-print len(no_repeats)
 with open('results.txt', 'a+') as output:
-    for index, item in enumerate(no_repeats):
-        print index,
-        try:
-            final_string = ''
-            r = requests.get(item)
-            final_string += item + '"'
-            if r.is_redirect or len(r.history) > 0 or r.is_permanent_redirect:
-                final_string += 'redirected ({})"'.format(r.url) + str(r.status_code)
-            elif r.status_code != 200:
-                if r.status_code == 404:
-                    final_string += '404"' + str(r.status_code)
+    for i in range(1, 3):
+        print str(i) + ' SITE STARTING'
+        output.write('SITE {}'.format(str(i)) + '\n')
+        xtree = get_tree('https://staterecords.org/sitemap{}.xml'.format(i))
+        urls = [el.text for el in xtree.xpath('//loc')]
+        print len(urls)
+        no_repeats = list(set(urls))
+        print len(no_repeats)
+
+        for index, item in enumerate(no_repeats):
+            print index,
+            try:
+                final_string = ''
+                r = requests.get(item)
+                final_string += item + '"'
+                if r.is_redirect or len(r.history) > 0 or r.is_permanent_redirect:
+                    final_string += 'redirected ({})"'.format(r.url) + str(r.status_code)
+                elif r.status_code != 200:
+                    if r.status_code == 404:
+                        final_string += '404"' + str(r.status_code)
+                    else:
+                        final_string += 'STATUS CODE"' + str(r.status_code)
                 else:
-                    final_string += 'STATUS CODE"' + str(r.status_code)
-            else:
-                final_string += 'GOOD"' + str(r.status_code)
-            output.write(final_string + '\n')
-            print final_string
-        except:
-            print 'REQUEST ERROR'
-            output.write(item + '"' + 'REQUEST ERROR')
+                    final_string += 'GOOD"' + str(r.status_code)
+                output.write(final_string + '\n')
+                print final_string
+            except:
+                print 'REQUEST ERROR'
+                output.write(item + '"' + 'REQUEST ERROR')
